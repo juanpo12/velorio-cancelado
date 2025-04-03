771bf2b062b7746eba89b327518bb8e96e842bb2
JuanPo <juanjoc2000@hotmail.com>
2025-04-03 20:27:10 -0300

    Add new page components for Cañuelas and Lomas de Zamora with header, footer, and main content sections

---
+++ app/canuelas/page.tsx
@@ -0,0 +1,26 @@
+import { ChangeSection } from "@/components/change-section";
+import { EventsSection } from "@/components/events-section";
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+import { HeroSection } from "@/components/hero-section";
+
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col bg-bg-velorio bg-cover">
+      {/* Header */}
+      <Header />
+
+      {/* Main Content */}
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <HeroSection />
+        <ChangeSection />
+        <EventsSection />
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  )
+}
+
---
+++ app/lomas-de-zamora/page.tsx
@@ -0,0 +1,25 @@
+import { Header } from "@/components/header"
+import { Footer } from "@/components/footer"
+import { HeroSection } from "@/components/hero-section"
+import { ChangeSection } from "@/components/change-section"
+import { EventsSection } from "@/components/events-section"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col bg-bg-velorio bg-cover">
+      {/* Header */}
+      <Header />
+
+      {/* Main Content */}
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <HeroSection />
+        <ChangeSection />
+        <EventsSection />
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  )
+}
+
